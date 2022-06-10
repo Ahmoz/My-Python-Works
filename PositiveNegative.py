77a6d35cae1d0abb4343500c87504d4c416ec4f1
Ahmoz <77580454+Ahmoz@users.noreply.github.com>
2022-06-10 21:48:49 +0300

    Add files via upload

---
+++ PositiveNegative.py
@@ -0,0 +1,21 @@
+#Positive or Negative ??
+
+
+#greeting
+print("Welcome!!!")
+print("**********")
+
+#input
+number1 = float(input("Please enter a number --->>>"))
+
+#checking and showing result
+if number1<0:
+    print("The number you entered is negative!!")
+elif number1>0:
+    print("The number you entered is positive!!")
+else:
+    print("The number you entered is 0 so it is neither negative nor positive")
+ 
+    
+#goodbye section
+print("See you later!!")
